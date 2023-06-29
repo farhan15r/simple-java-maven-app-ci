28af4dfcf07f9949fdbb06b0763189a3deb57458
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2023-06-29 19:20:14 +0700

    Deploy stage

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,3 +24,8 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
+
+echo 'Kill after 1 minute'
+sleep 60
+kill -9 $(lsof -t -i:8080)
+echo 'Killed'
